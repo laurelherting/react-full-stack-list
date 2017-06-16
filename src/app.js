8fbc68609c688ea32a1ba645080b05df13db43ff
Laurel Herting <laurel.herting@gmail.com>
2017-06-15 20:24:25 -0600

    wip modify values in array with map

--- src/app.js
+++ src/app.js
@@ -7,18 +7,48 @@ class App extends Component {
     this.onButton = this.onButton.bind(this);
   }
   onButton() {
-    console.log('HI');
+    console.log('Animal');
   }
   render() {
     return (
       <div>
-        <section>
-          <p>this is a list</p>
-        </section>
-        <input type="button" onClick={() => this.onButton()} />
+      <section>
+      <p>this is a list of animals</p>
+      </section>
+      <input type="button" onClick={() => this.onButton()} />
       </div>
     );
   }
 }
+
+const items = [
+  {
+    active: true,
+    popularname: 'hedgehog',
+    scientificname: 'Erinaceinae'
+  },
+  {
+    active: true,
+    popularname: 'cat',
+    scientificname: 'Felis catus'
+  },
+  {
+    active: false,
+    popularname: 'tiger',
+    scientificname: 'Panthera tigris'
+  }
+];
+
+const mapped = items
+  .filter(x => x.active) //
+  .map(x => x.popularname); // ['Shane', 'Sally']
+
+console.log("true");
+function createHtmlList(items) {
+  const listElements = items.map(x => <li>${x}</li>).join('\n');
+  return <ul>\n${listElements}\n</ul>;
+}
+
+createHtmlList(mapped);
 
 export default App;
