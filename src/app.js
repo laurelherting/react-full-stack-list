fa5f9ac2143fb921deb0a3a14dd34a1c81267960
Laurel Herting <laurel.herting@gmail.com>
2017-06-19 10:59:42 -0600

    fix scroll, change img

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ const items = [
   {
     id: 2,
     active: true,
-    popularname: 'cat',
-    scientificname: 'Felis catus',
+    popularname: 'tiger',
+    scientificname: 'Panthera tigris',
   },
   {
     id: 3,
     active: false,
-    popularname: 'tiger',
-    scientificname: 'Panthera tigris',
+    popularname: 'cat',
+    scientificname: 'Felis catus',
   },
 ];
 
@@ -35,11 +35,13 @@ class App extends Component {
     return (
       <div>
        <div className="header">
-          <img src="https://oup.useremarkable.com/production/images/uploads/2315/original/baby-hedgehog.jpg?147195945" alt="hedgehog" />
+        <img src="https://oup.useremarkable.com/production/images/uploads/2315/original/baby-hedgehog.jpg?147195945" alt="hedgehog" />
            <h2>Hedgehogs Rock!</h2>
+        <img src="https://s-media-cache-ak0.pinimg.com/originals/3c/20/63/3c2063499d5371f1212f1050d5577041.jpg" />
+        <img src="http://i.imgur.com/gbwgfw6.jpg" />
        </div>
           <section>
-            <p>Here is a list of my favorite animals scroll right-></p>
+            <p>Check out the list below to see my favorite animals!</p>
           </section>
           <input type="button" onClick={() => this.onButton()} />
         <ul>
