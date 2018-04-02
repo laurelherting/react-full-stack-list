7534a1add467d9199845ff7d56b73393af0c6ef8
Laurel Herting <laurel.herting@gmail.com>
2018-04-02 16:07:26 -0600

    add line

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const Express = require('express');
 
 const app = new Express();
 const PORT = process.env.PORT || 3000;
+
 app.use(Express.static(path.resolve(__dirname, 'dist')));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
