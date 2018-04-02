ffce3aa0207f004099a1a7f38be4d123c710f0f9
Laurel Herting <laurel.herting@gmail.com>
2018-04-02 15:00:47 -0600

    fix spacing

--- src/app.js
+++ src/app.js
@@ -37,28 +37,27 @@ class App extends Component {
   render() {
     return (
       <div>
-       <div className="header">
-        <img src="https://oup.useremarkable.com/production/images/uploads/2315/original/baby-hedgehog.jpg?147195945" alt="hedgehog" />
-           <h2>Hedgehogs, Tigers, Cats ... ROAR!!! (Make some noise Gizmo, Go!)</h2>
-        <img src="https://s-media-cache-ak0.pinimg.com/originals/3c/20/63/3c2063499d5371f1212f1050d5577041.jpg" />
-        <img src="http://i.imgur.com/gbwgfw6.jpg" />
-       </div>
-          <input type="button" value="Click here for more" onClick={() => this.onButton()} />
-          <section>
-
-      <h3>{this.state.message}</h3>
-            <p>Check out the list below to see my favorite animals!</p>
-          </section>
+        <div className="header">
+          <img src="https://oup.useremarkable.com/production/images/uploads/2315/original/baby-hedgehog.jpg?147195945" alt="hedgehog" />
+          <h2>Hedgehogs, Tigers, Cats ... ROAR!!! (Make some noise Gizmo, Go!)</h2>
+          <img src="https://s-media-cache-ak0.pinimg.com/originals/3c/20/63/3c2063499d5371f1212f1050d5577041.jpg" />
+          <img src="http://i.imgur.com/gbwgfw6.jpg" />
+        </div>
+        <input type="button" value="Click here for more" onClick={() => this.onButton()} />
+        <section>
+          <h3>{this.state.message}</h3>
+          <p>Check out the list below to see my favorite animals!</p>
+        </section>
         <ul>
           {items.map((x) => (
-            <li key={x.id}>{x.popularname} : {x.scientificname}</li>
-            ),
+          <li key={x.id}>{x.popularname} : {x.scientificname}</li>
+          ),
           )}
         </ul>
         <section>
           <p>Now it's your turn to add your favorite animals!</p>
         </section>
-      <LoginForm />
+        <LoginForm />
       </div>
     );
   }
