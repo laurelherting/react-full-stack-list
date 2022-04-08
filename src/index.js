b7e5166f2836b8dc1e6d362f010b4408cec995dc
Laurel Herting <laurel.herting@gmail.com>
2022-04-08 17:14:54 -0400

    update syntax

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './app';
-import LoginForm from './loginForm';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./app";
+import LoginForm from "./loginForm";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
